06efb01962f4601f5440ddb283102ecbed0d6c40
Ellonanew <ehllona@icloud.com>
2023-02-19 21:13:25 +0300

    module 1 task 1

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== 'number') {
+    throw new TypeError('argument a is not a number');
+  }
+  if (typeof b !== 'number') {
+    throw new TypeError('argument a is not a number');
+  }
+  return a + b;
 }
 
 module.exports = sum;
